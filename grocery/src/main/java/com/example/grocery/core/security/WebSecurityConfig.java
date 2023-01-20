265d31c7d7d9888afc6f44f984189963875de3be
Ömer Sarıtemur <palaomer3056@windowslive.com>
2023-01-21 02:24:37 +0300

    Security: SwaggerConfig created for UI testing

--- grocery/src/main/java/com/example/grocery/core/security/WebSecurityConfig.java
+++ grocery/src/main/java/com/example/grocery/core/security/WebSecurityConfig.java
@@ -101,5 +101,6 @@ public class WebSecurityConfig {
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+
     }
 }
