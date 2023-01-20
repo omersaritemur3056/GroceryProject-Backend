7c5ef8078a0991c2855b24a15c0a6a4e92f5ce68
Ömer Sarıtemur <palaomer3056@windowslive.com>
2023-01-21 02:09:07 +0300

    Security: SwaggerConfig added for authorization

---
+++ grocery/src/main/java/com/example/grocery/configs/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.grocery.configs;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    private String APP_TITLE = "Spring Security";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String apiTitle = String.format("%s API", StringUtils.capitalize(APP_TITLE));
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
+                .info(new Info().title(apiTitle).version("1"));
+    }
+}
