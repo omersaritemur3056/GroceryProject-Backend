4609306ccaadd08f24a3bc623228fc545283bf83
Ömer Sarıtemur <palaomer3056@windowslive.com>
2023-02-03 05:52:31 +0300

    Validation error handler designed

--- grocery/src/main/java/com/example/grocery/core/handleExceptions/GlobalExceptionHandler.java
+++ grocery/src/main/java/com/example/grocery/core/handleExceptions/GlobalExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.example.grocery.core.handleExceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,6 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import com.example.grocery.core.utilities.exceptions.BusinessException;
 import com.example.grocery.core.utilities.exceptions.TokenRefreshException;
+import com.example.grocery.core.utilities.results.ErrorDataResult;
 import com.example.grocery.core.utilities.results.ErrorResult;
 
 @RestControllerAdvice
@@ -24,5 +30,17 @@ public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.FORBIDDEN)
     public ErrorResult handleTokenRefreshException(TokenRefreshException tokenRefreshException, WebRequest webRequest) {
         return new ErrorResult(tokenRefreshException.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions) {
+        Map<String, String> validationErrors = new HashMap<>();
+
+        for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return new ErrorDataResult<>(validationErrors, "Validation errors!");
     }
 }
