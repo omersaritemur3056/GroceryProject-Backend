6d0684d3ce29d9db5a563ba7923b5eb4d7b722fd
Ömer Sarıtemur <palaomer3056@windowslive.com>
2023-04-16 07:22:56 +0300

    Fronend: Google login method created

---
+++ grocery/src/main/java/com/example/grocery/core/security/DTOs/request/GoogleLoginRequest.java
@@ -0,0 +1,2 @@
+package com.example.grocery.core.security.DTOs.request;public class GoogleLoginRequest {
+}
---
+++ grocery/src/main/java/com/example/grocery/core/security/enums/RegisterType.java
@@ -0,0 +1,2 @@
+package com.example.grocery.core.security.enums;public class RegisterType {
+}
